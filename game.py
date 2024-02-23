d04c579354df4ef80ed3004c696afa9507c6d37a
Dokong2 <jiwan6942@gmail.com>
2024-02-23 22:43:03 +0900

    하암ㅏ ㅑㅜ 이제 자야지ㅣ

--- game.py
+++ game.py
@@ -5,4 +5,11 @@ import pickle
 class data():
     def lanksave(data):
         with open("./gamepile/lank.pkl", "wb") as f:
-            pickle.dump()
+            pickle.dump(data, f)
+    def lanklode(data):
+        with open("./gamepile/lank.pkl", "rb") as f:
+            data = pickle.load(f)
+            return data
+
+def start():
+    pygame.display
