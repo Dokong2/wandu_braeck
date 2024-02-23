d8a57b649c69ebf5e6b1b7ef3ced805b2e8131f9
Dokong2 <jiwan6942@gmail.com>
2024-02-23 23:04:05 +0900

    진짜잘거임!

--- game.py
+++ game.py
@@ -1,15 +1,16 @@
 import pygame
 import threading
 import pickle
+import os
 
 class data():
     def lanksave(data):
         with open("./gamepile/lank.pkl", "wb") as f:
             pickle.dump(data, f)
-    def lanklode(data):
+    def lanklode():
         with open("./gamepile/lank.pkl", "rb") as f:
             data = pickle.load(f)
             return data
 
-def start():
-    pygame.display
+if __name__ == "__main__":
+    print(data.lanklode())
